c3753e4a0e63191838657cb36efe44d7f023b25e
Bo C. Herlin <bo@idipsum.se>
2015-11-27 16:13:16 +0100

    minor changes

--- makefile
+++ makefile
@@ -1,13 +1,13 @@
-.PHONY : all edit papers read clean tidy
+.PHONY : all edit pdf read clean tidy
 
 all: TARGET=all
 edit: TARGET=edit
-papers: TARGET=papers
+pdf: TARGET=pdf
 read: TARGET=read
 clean: TARGET=clean
 tidy: TARGET=tidy
 
-all edit papers read clean tidy :
+all edit pdf read clean tidy :
 	@exec $(MAKE) -C src $(TARGET)
 
 
--- src/submake.mk
+++ src/submake.mk
@@ -1,22 +1,22 @@
-PAPERS = ../../papers
+PDF = ../../pdf
 COMMON = ../common
 
-.PHONY: all edit paper read clean tidy test
+.PHONY: all edit pdf read clean tidy test
 
-all paper : ${PAPERS}/${FILENAME}.pdf
+all pdf : ${PDF}/${FILENAME}.pdf
 
-${PAPERS}/${FILENAME}.pdf : ${FILENAME}.tex *.png ${COMMON}/last-comment.tex ${COMMON}/elementary-physics.cls ${COMMON}/elementary-physics.bib
+${PDF}/${FILENAME}.pdf : ${FILENAME}.tex *.png ${COMMON}/last-comment.tex ${COMMON}/elementary-physics.cls ${COMMON}/elementary-physics.bib
 	pdflatex ${FILENAME}.tex
 	bibtex ${FILENAME}.aux
 	pdflatex ${FILENAME}.tex
 	pdflatex ${FILENAME}.tex
-	mv -f ${FILENAME}.pdf ${PAPERS}
+	mv -f ${FILENAME}.pdf ${PDF}
 
 edit :
 	gedit ${FILENAME}.tex &
 
-read : paper
-	evince ${PAPERS}/${FILENAME}.pdf &
+read : pdf
+	evince ${PDF}/${FILENAME}.pdf &
 
 tidy :
 	rm -f ${FILENAME}.aux
@@ -24,17 +24,18 @@ tidy :
 	rm -f ${FILENAME}.blg
 	rm -f ${FILENAME}.log
 	rm -f ${FILENAME}.out
+	rm -f ${FILENAME}.pdf
 	rm -f ${FILENAME}.run.xml
 	rm -f ${FILENAME}.toc
 	rm -f ${FILENAME}-blx.bib
 	rm -f *.tex~
 
 clean : tidy
-	rm -f ${PAPERS}/${FILENAME}.pdf
+	rm -f ${PDF}/${FILENAME}.pdf
 
 test :
 	pdflatex ${FILENAME}.tex
-	mv -f ${FILENAME}.pdf ${PAPERS}
-	evince ${PAPERS}/${FILENAME}.pdf &
+	mv -f ${FILENAME}.pdf ${PDF}
+	evince ${PDF}/${FILENAME}.pdf &
 
 
